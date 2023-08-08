28a07d1860d451ca84827fe689cb2320e9a019ef
XMLee <xmlee@un-net.com>
2023-08-08 18:02:52 +0800

    feat 整合nacos

--- xfzcode-genie-admin/xfzcode-genie-admin-system/src/main/java/com/xfzcode/genie/GenieAdminApplication.java
+++ xfzcode-genie-admin/xfzcode-genie-admin-system/src/main/java/com/xfzcode/genie/GenieAdminApplication.java
@@ -2,6 +2,7 @@ package com.xfzcode.genie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: XMLee
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GenieAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(GenieAdminApplication.class, args);
--- xfzcode-genie-admin/xfzcode-genie-admin-system/src/main/resources/sql/base.sql
+++ xfzcode-genie-admin/xfzcode-genie-admin-system/src/main/resources/sql/base.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost-3308
+ Source Server         : xfzcode-genie
  Source Server Type    : MySQL
- Source Server Version : 80012
- Source Host           : localhost:3308
- Source Schema         : genie_admin
+ Source Server Version : 50742
+ Source Host           : 47.101.151.24:13306
+ Source Schema         : xfzode-genie-admin
 
  Target Server Type    : MySQL
- Target Server Version : 80012
+ Target Server Version : 50742
  File Encoding         : 65001
 
- Date: 25/07/2023 09:22:41
+ Date: 08/08/2023 16:03:56
 */
 
 SET NAMES utf8mb4;
@@ -22,32 +22,46 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_permission`;
 CREATE TABLE `sys_permission`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `menu_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'NULL' COMMENT '菜单名',
-  `path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '路由地址',
-  `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '组件路径',
-  `visible` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '菜单状态（0显示 1隐藏）',
-  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '菜单状态（0正常 1停用）',
-  `perms` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限标识',
-  `create_by` bigint(20) NULL DEFAULT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `update_by` bigint(20) NULL DEFAULT NULL,
-  `update_time` datetime(0) NULL DEFAULT NULL,
-  `del_flag` int(11) NULL DEFAULT 0 COMMENT '是否删除（0未删除 1已删除）',
-  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `parent_id` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
+  `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单标题',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '路径',
+  `component` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '组件',
+  `component_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '组件名字',
+  `menu_type` int(11) NULL DEFAULT NULL COMMENT '菜单类型(0:一级菜单; 1:子菜单:2:按钮权限)',
+  `status` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '按钮权限状态(0无效1有效)',
+  `perms` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单权限编码',
+  `perms_type` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0' COMMENT '权限策略1显示2禁用',
+  `sort_no` double(8, 2) NULL DEFAULT NULL COMMENT '菜单排序',
+  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
+  `is_leaf` tinyint(1) NULL DEFAULT NULL COMMENT '是否叶子节点:    1是0否',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `del_flag` int(11) NULL DEFAULT 0 COMMENT '删除状态 0正常 1已删除',
+  `parent_id` bigint(20) NULL DEFAULT NULL COMMENT '父id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_menu_type`(`menu_type`) USING BTREE,
+  INDEX `index_menu_status`(`status`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '菜单权限表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission` VALUES (1, 'string', 'string', 'perminssion', '权限ddd管理', 0, '0', 'string', '0', 3.00, 'string', 0, 'string', NULL, NULL, 0, 0);
+INSERT INTO `sys_permission` VALUES (2, 'string', 'string', 'perminssio1n', '权限管理1', 0, '0', 'string', '0', 2.00, 'string', 0, 'string', NULL, NULL, 0, 0);
+INSERT INTO `sys_permission` VALUES (3, 'string', 'string', 'perminssio1n22', '权限管理122', 0, '0', 'string', '0', 2.00, 'string', 0, 'string', NULL, NULL, 0, 2);
 
 -- ----------------------------
 -- Table structure for sys_role_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_permission`;
 CREATE TABLE `sys_role_permission`  (
-  `role_id` bigint(200) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
-  `permission_id` bigint(200) NOT NULL DEFAULT 0 COMMENT '菜单id',
-  PRIMARY KEY (`role_id`, `permission_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `id` bigint(32) NULL DEFAULT NULL,
+  `role_id` bigint(32) NULL DEFAULT NULL COMMENT '角色ID',
+  `permission_id` bigint(32) NOT NULL DEFAULT 0 COMMENT '菜单id',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_roles
@@ -55,14 +69,14 @@ CREATE TABLE `sys_role_permission`  (
 DROP TABLE IF EXISTS `sys_roles`;
 CREATE TABLE `sys_roles`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `create_timestamp` datetime(0) NOT NULL,
-  `update_timestamp` datetime(0) NOT NULL,
+  `create_time` datetime(0) NOT NULL,
+  `update_time` datetime(0) NOT NULL,
   `authority` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '描述',
   `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `UK_ccwsk2180xxig731jlcidpi7p`(`authority`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_users
@@ -70,21 +84,24 @@ CREATE TABLE `sys_roles`  (
 DROP TABLE IF EXISTS `sys_users`;
 CREATE TABLE `sys_users`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号',
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `create_timestamp` datetime(0) NOT NULL COMMENT '创建时间',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `enabled` bit(1) NOT NULL COMMENT '账号是否启用：1正常',
   `account_non_locked` bit(1) NOT NULL COMMENT '账号是否锁定：1正常',
-  `update_timestamp` datetime(0) NOT NULL COMMENT '更新时间',
+  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
   `account_expire_date` datetime(0) NULL DEFAULT NULL,
   `credential_expire_date` datetime(0) NULL DEFAULT NULL,
   `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',
   `real_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '真实名称',
-  `area_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `department` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `UK_lgllb14jd9kcm09b9enkkbh0q`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `UK_lgllb14jd9kcm09b9enkkbh0q`(`user_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_users
+-- ----------------------------
+INSERT INTO `sys_users` VALUES (1, '19936063808', '$2a$12$HA4l/Z5UTR5QZN7vovNTTu/7WJfeJtvoihF7.t5de6y/st06s.Bri', '2023-08-08 14:57:00', b'1', b'1', '2023-08-08 14:57:00', '2100-01-30 00:00:00', '2100-01-30 00:00:00', '19936063808', NULL);
 
 -- ----------------------------
 -- Table structure for sys_users_roles
@@ -97,6 +114,6 @@ CREATE TABLE `sys_users_roles`  (
   `role_id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 84 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
